63b65cca2b085120a560895cb347e350523f2f79
AlexandrePaes <alexpaes.itsolutions@gmail.com>
2020-11-21 23:35:17 -0400

    INITIAL COMMIT BUGGY: Initial commits were buggy but this fixed now

---
+++ portfolio_project/alexandrepaes_portfolio_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AlexandrepaesPortfolioAppConfig(AppConfig):
+    name = 'alexandrepaes_portfolio_app'
---
+++ portfolio_project/alexandrepaes_portfolio_blog/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AlexandrepaesPortfolioBlogConfig(AppConfig):
+    name = 'alexandrepaes_portfolio_blog'
